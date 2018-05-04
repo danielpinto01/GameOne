c09e48e188d21fec42769252953464d8194a0a12
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-05-03 22:40:07 -0500

    Se agregaron metodos a la clase Player

--- src/models/Player.java
+++ src/models/Player.java
@@ -1,5 +1,49 @@
 package models;
 
-public class Player {
+import java.util.Random;
+import models.DirectionPlayer;
 
+public class Player extends MyThread{
+	
+	private static final int MOVE_UNITS = 50;
+	private int x;
+	private int y;
+	
+	public Player() {
+		super("player");
+		x = new Random().nextInt(500);
+		y = new Random().nextInt(500);
+		start();
+	}
+	
+	public void move(DirectionPlayer directionPlayer){
+		switch (directionPlayer) {
+		case DOWN:
+			y += MOVE_UNITS;
+			break;
+		case LEFT:
+			x -= MOVE_UNITS;
+			break;
+		case RIGHT:
+			x += MOVE_UNITS;
+			break;
+		case UP:
+			y -= MOVE_UNITS;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	void executeTask() {
+	}
 }
