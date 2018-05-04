5f765dc769257cb84e3f6df5b4b52686ee8dd507
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-05-03 22:36:33 -0500

    Se agregaron los metodos a la clase myThread

--- src/models/MyThread.java
+++ src/models/MyThread.java
@@ -1,5 +1,63 @@
 package models;
 
-public class MyThread {
+public abstract class MyThread implements Runnable{
 	
+	private Thread thread;
+	private String name;
+	private boolean stop;
+	private boolean pause;
+	
+	public MyThread(String name) {
+		this.name = name;
+		thread = new Thread(this, this.name);
+	}
+	
+	public void start(){
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		pause = false;
+		stop = true;
+		notify();
+		
+	}
+	
+	public synchronized void pause(){
+		pause = true;
+	}
+	
+	public synchronized void resume(){
+		pause = false;
+		notify();
+	}
+	
+	@Override
+	public void run() {
+		while (!stop) {
+			executeTask();
+			synchronized (this) {
+				while (pause) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				if (stop)
+					break;
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	abstract void executeTask();
+	
+	public Thread getThread() {
+		return thread;
+	}
 }
